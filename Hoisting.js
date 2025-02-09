a185929f6ac2e0c2a45098987988d34eeb747ed6
Naushad Ali <naushadshaikhjintur@gmail.com>
2025-02-09 19:52:17 +0530

    Hoisting

---
+++ Hoisting.js
@@ -0,0 +1,13 @@
+
+getName()
+console.log(x);
+const x=2;
+function getName(){
+    console.log("Hello I am ali !");
+    
+}
+
+// getName;
+// console.log(x);
+
+// accessing variable and function even before initializing them called as hoisting.
